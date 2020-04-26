c58710fe21c584182de0e013dd2cd795cab73467
3lVv0w <krseisenh@gmail.com>
2020-04-26 21:46:30 +0700

    add firebase product

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
+import 'dart:async';
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:own_dog/views/own_dog.dart';
 
-void main() => runApp(OwnDog());
+void main() {
+  Crashlytics.instance.enableInDevMode = true;
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+  runZoned(
+    () {
+      runApp(OwnDog());
+    },
+    onError: Crashlytics.instance.recordError,
+  );
+}
--- lib/views/own_dog.dart
+++ lib/views/own_dog.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:own_dog/views/pages/login_page.dart';
 
 class OwnDog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FirebaseAnalytics analytics = FirebaseAnalytics();
+
     return MaterialApp(
       title: 'Owner Dog',
       theme: ThemeData(
@@ -11,6 +15,9 @@ class OwnDog extends StatelessWidget {
       ),
       home: LoginPage(),
       debugShowCheckedModeBanner: false,
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analytics),
+      ],
     );
   }
 }
